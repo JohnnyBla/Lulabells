623e70a78697a06626f0a0fd98b6334d424eac06
Johnny Blakley <majhack1990@gmail.com>
2022-10-12 08:31:27 -0400

    animation

--- src/Components/LandingPage/sectionGroupTwo/MenuList.js
+++ src/Components/LandingPage/sectionGroupTwo/MenuList.js
@@ -1,26 +1,35 @@
 import React, { useState } from 'react';
-
 import { MDBIcon } from 'mdb-react-ui-kit';
+import { useSpring, animated } from 'react-spring';
 
 const MenuList = ({ name, id }) => {
   const [expanded, setExpanded] = useState(false);
+  const styles = useSpring({
+    from: { x: 600 },
+    config: { duration: 2000 },
+    loop: {
+      x: 0,
+    },
+  });
 
   return (
-    <article className='question'>
-      <header>
-        <h4 onClick={() => setExpanded(!expanded)} className='question-title'>
-          {name}
-        </h4>
-        <button className='btnType' onClick={() => setExpanded(!expanded)}>
-          {expanded ? (
-            <MDBIcon fas icon='times-circle' size='2x' />
-          ) : (
-            <MDBIcon fas icon='plus-circle' size='2x' />
-          )}
-        </button>
-      </header>
-      {expanded && <p>{id}</p>}
-    </article>
+    <animated.div style={{ ...styles }}>
+      <article className='question'>
+        <header>
+          <h4 onClick={() => setExpanded(!expanded)} className='question-title'>
+            {name}
+          </h4>
+          <button className='btnType' onClick={() => setExpanded(!expanded)}>
+            {expanded ? (
+              <MDBIcon fas icon='times-circle' size='2x' />
+            ) : (
+              <MDBIcon fas icon='plus-circle' size='2x' />
+            )}
+          </button>
+        </header>
+        {expanded && <p>{id}</p>}
+      </article>
+    </animated.div>
   );
 };
 
